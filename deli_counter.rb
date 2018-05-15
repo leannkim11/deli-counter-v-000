b2c000aacaf5885edef97ef06e47ef7f473a1091
Leann Kim <leannminakim@gmail.com>
2018-05-15 16:36:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+katz_deli = []
+def line (customer)
+  line_array =[]
+  if customer.length == 0
+    puts "The line is currently empty."
+  else
+    customer.each_with_index do |name, index|
+      line_array << "#{index + 1}. #{name}"
+    end
+    puts "this line is currently: #{line_array.join(" ")}"
+  end
+end
+
+def take_a_number
+
+
+end
+
+def now_serving
+
+end
